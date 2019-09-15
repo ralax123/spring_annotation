999f7d15dcc853d06ce18c9979bf7cb6f10639ee
helloworld <1025599740@qq.com>
2019-09-15 13:20:46 +0800

    添加测试类

--- src/main/java/com/itcast/service/UserServiceImpl.java
+++ src/main/java/com/itcast/service/UserServiceImpl.java
@@ -4,7 +4,7 @@ import com.itcast.dao.UserDao;
 import com.itcast.dao.UserDaoImpl;
 import org.springframework.stereotype.Service;
 
-@Service()
+@Service("userService1")
 public class UserServiceImpl implements UserService {
     private UserDao userDao = new UserDaoImpl();
 
